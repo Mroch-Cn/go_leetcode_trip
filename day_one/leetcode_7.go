021266577f4c4e1a27c9ac71372207df97ac1fb9
cuizhongxi <cuizx@jidemail.com>
2020-03-15 14:23:43 +0800

    leetcode_7

---
+++ day_one/leetcode_7.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+func main() {
+	reverse(345)
+}
+
+func reverse(x int) int {
+	var min = math.MinInt32
+	var max = math.MaxInt32
+	var y int = 0
+	for x != 0 {
+		y = y*10 + x%10
+		x = x / 10
+		if y < min || y > max {
+			return 0
+		}
+	}
+	return y
+}
